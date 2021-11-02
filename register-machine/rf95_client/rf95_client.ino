d5d685ce95804bcef8e6f2c16d3f94a8bf51a349
Ben Burdette <bburdette@gmail.com>
2021-11-02 13:08:56 -0600

    starting with copies of the rf95 sketches

---
+++ register-machine/rf95_client/rf95_client.ino
@@ -0,0 +1,70 @@
+// rf95_client.pde
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple messageing client
+// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
+// reliability, so you should only use RH_RF95 if you do not need the higher
+// level messaging abilities.
+// It is designed to work with the other example rf95_server
+// Tested with Automato Sensor Module.
+
+#include <SPI.h>
+#include <RH_RF95.h>
+
+RH_RF95 rf95(PIN_LORA_CS, PIN_LORA_IRQ); // Slave select, interrupt pin for Automato Sensor Module.
+
+void setup() 
+{
+  pinMode(PIN_LORA_RST, INPUT); // Let the pin float.
+
+  // Disable other SPI devices.
+  pinMode(PIN_LCD_CS, OUTPUT);
+  digitalWrite(PIN_LCD_CS, HIGH);
+  pinMode(PIN_TCH_CS, OUTPUT);
+  digitalWrite(PIN_TCH_CS, HIGH);
+  pinMode(PIN_SD_CS, OUTPUT);
+  digitalWrite(PIN_SD_CS, HIGH);
+  
+  Serial.begin(115200);
+  //while (!Serial) ; // Wait for serial port to be available
+  Serial.println("Initializing LoRa"); 
+  if (!rf95.init())
+    Serial.println("init failed");
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+  rf95.setFrequency(915.0);
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // You can set transmitter powers from 5 to 23 dBm:
+  //  driver.setTxPower(23);
+}
+
+void loop()
+{
+  Serial.println("Sending to rf95_server");
+  // Send a message to rf95_server
+  float sample_data = 1.23;
+  rf95.send((uint8_t*)&sample_data, sizeof(sample_data));
+  
+  rf95.waitPacketSent();
+  // Now wait for a reply
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+
+  if (rf95.waitAvailableTimeout(3000))
+  { 
+    // Should be a reply message for us now   
+    if (rf95.recv(buf, &len))
+   {
+      Serial.print("got reply: ");
+      Serial.println((char*)buf);
+    }
+    else
+    {
+      Serial.println("recv failed");
+    }
+  }
+  else
+  {
+    Serial.println("No reply, is rf95_server running?");
+  }
+  delay(400);
+}
