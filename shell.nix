b34fc62f0098817f36b9c8b675738b918991e2b4
Ben Burdette <>
2025-10-15 11:00:44 -0600

    comment

--- shell.nix
+++ shell.nix
@@ -1,4 +1,9 @@
 # shell for arduino dev on nixos
+#
+# put this in a directory and `nix-shell` to get these tools.
+#
+# or if you use direnv, make a .envrc containing `use_nix` for automatic loading when you cd
+# to the directory.
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     nativeBuildInputs = [
